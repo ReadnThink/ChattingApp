de7ce268ffcd5351d3815d2f07d9811ceec96f8b
solbae <sol4854@naver.com>
2022-12-19 10:22:24 +0900

    first commit

---
+++ src/main/java/com/chat/chattingapp/controller/RsData.java
@@ -0,0 +1,10 @@
+package com.chat.chattingapp.controller;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class RsData {
+    private String resultCode;
+    private String msg;
+    private String data;
+}
